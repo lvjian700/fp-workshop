ae5b96bc7aa4465cf5ea2e92170a23ab306dba4f
lvjian700 <lvjian700@gmail.com>
2014-01-14 15:08:19 +0800

    Jian - first import, provide underscore

---
+++ index.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+console.log('Xi\'an thoughtworks Functional Javascript Programming workshop');
+
+_.each(['Please', 'feel', 'free', 'to join us.'], function(item) {
+	console.log('>' + item);	
+});
+
+
+console.log('Your env is Ok!');
